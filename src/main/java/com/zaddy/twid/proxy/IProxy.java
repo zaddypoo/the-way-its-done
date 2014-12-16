8c339f8436d2b6db8d834a1497f1b230b1bdce4a
zaddypoo <krazykris47421@gmail.com>
2014-12-16 16:23:03 -0500

    First code commit

---
+++ src/main/java/com/zaddy/twid/proxy/IProxy.java
@@ -0,0 +1,7 @@
+package com.zaddy.twid.proxy;
+
+/**
+ * Created by Zaddy on 12/16/2014.
+ */
+public interface IProxy {
+}
---
+++ src/main/java/com/zaddy/twid/reference/Reference.java
@@ -0,0 +1,7 @@
+package com.zaddy.twid.reference;
+
+/**
+ * Created by Zaddy on 12/16/2014.
+ */
+public class Reference {
+}
